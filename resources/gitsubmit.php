24cfb24faf8c87f5ee709261c40112283a052cc4
nmarcopo <nmarcopo@nd.edu>
2018-08-24 10:32:33 -0400

    fixed formatting and added alternate URL suggestion

--- resources/gitsubmit.php
+++ resources/gitsubmit.php
@@ -96,7 +96,7 @@ function createShortLink($url, $code){
     if($header == "https://git.io/$code"){
         return "Vanity URL has been created for <a href=$url>$url</a> at <a href=$header>$header</a>.";
     }else{
-        return "Vanity URL already exists for <a href=$url>$url</a> (<a href=$header>$header</a>) - you cannot create more than one vanity URL for the same page.<br>Tip: add an empty query (a question mark ('?')) to create a vanity URL for this page.";
+        return "Vanity URL already exists for <a href=$url>$url</a> (<a href=$header>$header</a>) - you cannot create more than one vanity URL for the same page.<br>Tip: add an empty query (a question mark ('?')) to create a vanity URL for this page.<br>i.e. copy and paste this link into the GitHub URL field and try again:<br><a href=\"$url?\">$url?</a>";
     }
 
 }
