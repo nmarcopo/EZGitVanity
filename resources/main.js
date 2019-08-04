aabc8c66f9bb154245949165b04f5a1eb2ee9655
Nicholas Marcopoli <nick@marcopo.li>
2019-08-03 21:43:00 -0700

    improved shortlink entering and added error modal

--- resources/main.js
+++ resources/main.js
@@ -12,8 +12,9 @@ document.getElementById("checkCodeAvailable").addEventListener('click', function
 // Function for second button
 document.getElementById("requestGitIo").addEventListener('click', function () {
     let githubURL = document.getElementById("githubURL").value
-    if(!(/https?:\/\//g.test(githubURL))){
-        outputBlock.innerHTML = "Please format your original GitHub URL using http:// or https://.";
+    if(!(/https?:\/\/.+\..+/g.test(githubURL))){
+        // Sadly we need jQuery to trigger the modal
+        $("#invalidModal").modal('show')
         return;
     }
 
@@ -26,32 +27,26 @@ document.getElementById("requestGitIo").addEventListener('click', function () {
         },
         body: "url=" + $("#githubURL").val() + "&code=" + $("#gitIoCode").val(),
     }).then(function () {
-        outputBlock.innerHTML = "Request for custom URL sent! Please check to make sure that <a href=https://git.io/" + gitIoCode + " target=blank>https://git.io/" + gitIoCode + "</a> leads to your link. If it doesn't, verify that your vanity URL term is available.";
+        outputBlock.innerHTML = "Request for custom URL sent! Please check to make sure that <a href=https://git.io/" + gitIoCode + " target=blank>https://git.io/" + gitIoCode + "</a> leads to your link. If it doesn't, verify that your vanity URL term is available, or check <a href=https://github.com/nmarcopo/EZGitVanity#faq target=blank>our FAQs</a> for more help.";
     });
 });
 
-// Only make "Check Availability" button available when text is entered in the 'code' section
-function toggleAvailableButton() {
-    document.getElementById("checkCodeAvailable").disabled = $("#gitIoCode").val() === "";
-}
-
 // Only make "Request Vanity URL" button available when text is entered in both the 'code' and 'githubURL' section
 function toggleRequestButton() {
-    document.getElementById("requestGitIo").disabled = $("#githubURL").val() === "" || $("#gitIoCode").val() === "";
+    document.getElementById("requestGitIo").disabled = document.getElementById("githubURL").value === "" || document.getElementById("gitIoCode").value === "";
 }
 
 // Encode the 'code' so that it is compatible with the URL shortener
 function encodeURL() {
-    document.getElementById("gitIoCode").value = decodeURI(document.getElementById("gitIoCode").value);
-    document.getElementById("gitIoCode").value = encodeURI(document.getElementById("gitIoCode").value);
+    let gitIoCode = document.getElementById("gitIoCode").value;
+    gitIoCode = encodeURI(gitIoCode);
 
     // Modify "output" text
-    if (document.getElementById("gitIoCode").value !== '') {
-        outputBlock.innerHTML = `Check availability for the URL: https://git.io/${document.getElementById("gitIoCode").value}`;
+    if (gitIoCode !== '') {
+        outputBlock.innerHTML = "Check to make sure that <a href=https://git.io/" + gitIoCode + " target=blank>https://git.io/" + gitIoCode + "</a> contains the message:<br><code>No url found for " + gitIoCode + "</code><br>If it does, then you can place a request! Otherwise, please choose another vanity URL term.";
     } else {
         outputBlock.innerHTML = 'Please enter a Vanity URL term.';
     }
 
     toggleRequestButton();
-    toggleAvailableButton();
 }
